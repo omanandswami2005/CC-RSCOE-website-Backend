184ca3a2abf9493de39d10330864529aea459758
omanand <omanandswami2005@gmail.com>
2025-03-10 18:57:39 +0530

    fix:2

--- services/auth-service/src/utils/generateToken.ts
+++ services/auth-service/src/utils/generateToken.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import config from "../../../../shared/src/configs/env";
+import config from "../config/env";
 
 export const generateToken = (userId: string): string => {
   return jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: parseInt(config.TOKEN_EXPIRES_IN, 10) });
